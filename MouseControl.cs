ddcecd48449cca8756ef6215a4e0dae9c18e952e
KaviGame <78657200+KaviGame@users.noreply.github.com>
2021-02-06 22:53:16 +0530

    Add files via upload

---
+++ MouseControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform player;
+
+    float xRotation = 0f;
+
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update ()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
