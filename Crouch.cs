f2cb47df283ad095b545df4df90222a33340c8ba
KaviGame <78657200+KaviGame@users.noreply.github.com>
2021-02-13 21:25:38 +0530

    Add files via upload

---
+++ Crouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public CapsuleCollider player;
+    float originalHeight;
+    public float reducedHeight;
+
+    void Start ()
+    {
+        originalHeight = player.height;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            GetDown();
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+            GetUp();
+    }
+
+    void GetDown ()
+    {
+        player.height = reducedHeight;
+    }
+
+    void GetUp ()
+    {
+        player.height = originalHeight;
+    }
+}
