0d226a8ab538185a5b96074cc5283e6c66161bf5
KaviGame <78657200+KaviGame@users.noreply.github.com>
2021-03-02 15:21:21 +0530

    Add files via upload

---
+++ CountDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public Text textBox;
+    public float timeStart = 30;
+
+    void Start ()
+    {
+        textBox.text = timeStart.ToString();
+    }
+
+    void Update ()
+    {
+        timeStart -= Time.deltaTime;
+        textBox.text = Mathf.Round(timeStart).ToString();
+    }
+}
